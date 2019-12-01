9ac6d290197836c530f99b11561a3e12982207ba
Carl Edwin <carlinstr@gmail.com>
2019-11-30 23:48:50 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/wordpress/carledwinti/sistema/ponto/eletronico/api/SistemaPontoEletronicoApiApplication.java
@@ -0,0 +1,13 @@
+package com.wordpress.carledwinti.sistema.ponto.eletronico.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPontoEletronicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPontoEletronicoApiApplication.class, args);
+	}
+
+}
