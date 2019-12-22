bc059eedbb20c1020e70abce393f283b6030683f
Carl Edwin <carlinstr@gmail.com>
2019-12-22 14:41:13 -0300

    Criação do script para acessar base de dados remota

---
+++ src/main/resources/scripts/database-heroku.sql
@@ -0,0 +1,27 @@
+/*
+connection: Heroku
+host: us-cdbr-iron-east-05.cleardb.net
+port: 3306
+username: ******
+password: ******
+database: heroku_1d8ce0cd12bd920
+*/
+
+/*
+show databases;
+*/
+/*
+use heroku_1d8ce0cd12bd920;
+*/
+/*
+show tables;
+*/
+/*
+select * from empresa;
+*/
+
+select * from funcionario;
+
+/*
+select * from lancamento;
+*/
